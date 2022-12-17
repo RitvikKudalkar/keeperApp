3e4c6824ed3c13740a516c67b04a3a5ac7f765fd
RitvikKudalkar <ritxxx77@gmail.com>
2022-12-17 13:24:00 +0530

    first commit

---
+++ build/precache-manifest.b6fcb6f5c887e13286223a33b2066114.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc13919dab6a2cfe1f9789611857c79",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "eefa04d0a7e15799a845",
+    "url": "/KeeperApp/static/js/2.05f7ee33.chunk.js"
+  },
+  {
+    "revision": "1ec089b99fc6c5776228",
+    "url": "/KeeperApp/static/js/main.862061dd.chunk.js"
+  },
+  {
+    "revision": "78e85e04716e09c6db33",
+    "url": "/KeeperApp/static/js/runtime-main.1ca87832.js"
+  }
+]);
